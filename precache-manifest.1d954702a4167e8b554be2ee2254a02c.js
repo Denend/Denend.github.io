c2abcac73632af3a37150b65d8e75ce0ccfc53a7
Danylo Soloviov <denend.soloviov@gmail.com>
2019-01-08 20:14:14 +0200

    Updates

---
+++ precache-manifest.1d954702a4167e8b554be2ee2254a02c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf30426440676e8e5b40",
+    "url": "/myResiptsPrj/static/js/runtime~main.bf304264.js"
+  },
+  {
+    "revision": "d42488cf42039b3f2912",
+    "url": "/myResiptsPrj/static/js/main.d42488cf.chunk.js"
+  },
+  {
+    "revision": "1ae0bf0dadfb20006aeb",
+    "url": "/myResiptsPrj/static/js/1.1ae0bf0d.chunk.js"
+  },
+  {
+    "revision": "d42488cf42039b3f2912",
+    "url": "/myResiptsPrj/static/css/main.2b10a835.chunk.css"
+  },
+  {
+    "revision": "dbac7faff670d9a4bf9a7e6cd9cccf83",
+    "url": "/myResiptsPrj/index.html"
+  }
+];
